6cfcf6bf6ac0ba8eff0c78feeed0944fa4871b06
Matt Rice <mrice1@midmich.edu>
2014-06-20 12:10:52 -0400

    Fix recent users

--- mmcc/recent_users.php
+++ mmcc/recent_users.php
@@ -7,10 +7,11 @@
 //
 //Modified version of /var/moodle/recent_users.php from mapp3, written by Brandon Kish
 
+require_once('constants.php');
 
 // Connect to database
-$conn = mysql_connect('db.midmich.edu', 'pass', '79mNgXlGLogjifu') or die("Database connection failed");
-$db = mysql_select_db('moodle2_pass') or die("Database selection failed");
+$conn = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die("Database connection failed");
+$db = mysql_select_db(DB_NAME) or die("Database selection failed");
 
 $sql = "SELECT COUNT(*) AS recent_users FROM mdl_user WHERE lastaccess >= " . (time() - (60 * 5));
 $rs = mysql_query($sql) or die ("Query failed: $sql");
