484eb61cebe9d72a3a0868438c3c8745824d318f
Matt Rice <mrice1@midmich.edu>
2014-06-23 16:17:47 -0400

    Changed front page to no longer display a list of all available courses when user had no enrolled courses to display
    (cherry picked from commit f99d41056031002dc24a99ba8dc713a80e184149)

--- course/renderer.php
+++ course/renderer.php
@@ -2061,25 +2061,7 @@ class core_course_renderer extends plugin_renderer_base {
      * @return string
      */
     public function frontpage_available_courses() {
-        global $CFG;
-        require_once($CFG->libdir. '/coursecatlib.php');
-
-        $chelper = new coursecat_helper();
-        $chelper->set_show_courses(self::COURSECAT_SHOW_COURSES_EXPANDED)->
-                set_courses_display_options(array(
-                    'recursive' => true,
-                    'limit' => $CFG->frontpagecourselimit,
-                    'viewmoreurl' => new moodle_url('/course/index.php'),
-                    'viewmoretext' => new lang_string('fulllistofcourses')));
-
-        $chelper->set_attributes(array('class' => 'frontpage-course-list-all'));
-        $courses = coursecat::get(0)->get_courses($chelper->get_courses_display_options());
-        $totalcount = coursecat::get(0)->get_courses_count($chelper->get_courses_display_options());
-        if (!$totalcount && !$this->page->user_is_editing() && has_capability('moodle/course:create', context_system::instance())) {
-            // Print link to create a new course, for the 1st available category.
-            return $this->add_new_course_button();
-        }
-        return $this->coursecat_courses($chelper, $courses, $totalcount);
+        return '';
     }
 
     /**
