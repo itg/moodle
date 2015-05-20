6d0ae9d851a8061c6e087ddf3c12eb2e86838d08
Matt Rice <mrice1@midmich.edu>
2015-05-20 12:17:52 -0400

    Changes to available 'title' sizes
    -Added h1, h2, h6
    -Changed menu appearance for available options
    
    (cherry picked from commit 1bb823828b1df248d5779e14b5c2b4d35f13a649)

--- lib/editor/atto/plugins/title/lib.php
+++ lib/editor/atto/plugins/title/lib.php
@@ -27,9 +27,12 @@ defined('MOODLE_INTERNAL') || die();
 function atto_title_strings_for_js() {
     global $PAGE;
 
-    $PAGE->requires->strings_for_js(array('h3',
+    $PAGE->requires->strings_for_js(array('h1',
+                                          'h2',
+                                          'h3',
                                           'h4',
                                           'h5',
+                                          'h6',
                                           'pre',
                                           'p'), 'atto_title');
 }
