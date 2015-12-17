dfa27716a3b48097c02687929d9d938798580a5c
Matt Rice <mrice1@midmich.edu>
2015-12-17 16:43:47 -0500

    MDL-46881 mod_forum: fix php coding style problems

--- mod/forum/lib.php
+++ mod/forum/lib.php
@@ -913,37 +913,37 @@ function forum_mail_digests_task() {
     }
     $digestsset->close();
 
-    // Now see if there are any digest mails waiting to be sent, and if we should send them
+    // Now see if there are any digest mails waiting to be sent, and if we should send them.
 
     mtrace('Starting digest processing...');
 
-    core_php_time_limit::raise(300); // terminate if not able to fetch all digests in 5 minutes
+    core_php_time_limit::raise(300); // Terminate if not able to fetch all digests in 5 minutes.
 
-    if (!isset($CFG->digestmailtimelast)) {    // To catch the first time
+    if (!isset($CFG->digestmailtimelast)) {    // To catch the first time.
         set_config('digestmailtimelast', 0);
     }
 
     $timenow = time();
 
-    // Delete any really old ones (normally there shouldn't be any)
+    // Delete any really old ones (normally there shouldn't be any).
     $weekago = $timenow - (7 * 24 * 3600);
     $DB->delete_records_select('forum_queue', "timemodified < ?", array($weekago));
     mtrace ('Cleaned old digest records');
 
     mtrace('Sending forum digests: '.userdate($timenow, '', $sitetimezone));
 
-    $digestposts_rs = $DB->get_recordset('forum_queue');
+    $digestposts = $DB->get_recordset('forum_queue');
 
-    if ($digestposts_rs->valid()) {
+    if ($digestposts->valid()) {
 
-        // We have work to do
+        // We have work to do.
         $usermailcount = 0;
 
-        //caches - reuse the those filled before too
+        // Caches - reuse the those filled before too.
         $discussionposts = array();
         $userdiscussions = array();
 
-        foreach ($digestposts_rs as $digestpost) {
+        foreach ($digestposts as $digestpost) {
             if (!isset($posts[$digestpost->postid])) {
                 if ($post = $DB->get_record('forum_posts', array('id' => $digestpost->postid))) {
                     $posts[$digestpost->postid] = $post;
@@ -987,18 +987,18 @@ function forum_mail_digests_task() {
             $userdiscussions[$digestpost->userid][$digestpost->discussionid] = $digestpost->discussionid;
             $discussionposts[$digestpost->discussionid][$digestpost->postid] = $digestpost->postid;
         }
-        $digestposts_rs->close(); /// Finished iteration, let's close the resultset
+        $digestposts->close(); // Finished iteration, let's close the resultset.
 
-        // Data collected, start sending out emails to each user
+        // Data collected, start sending out emails to each user.
         foreach ($userdiscussions as $userid => $thesediscussions) {
 
-            core_php_time_limit::raise(120); // terminate if processing of any account takes longer than 2 minutes
+            core_php_time_limit::raise(120); // Terminate if processing of any account takes longer than 2 minutes.
 
             cron_setup_user();
 
             mtrace(get_string('processingdigest', 'forum', $userid), '... ');
 
-            // First of all delete all the queue entries for this user
+            // First of all delete all the queue entries for this user.
             $DB->delete_records_select('forum_queue', "userid = ?", array($userid));
 
             // Init user caches - we keep the cache for one cycle only,
@@ -1024,10 +1024,11 @@ function forum_mail_digests_task() {
             $headerdata->userprefs = $CFG->wwwroot.'/user/edit.php?id='.$userid.'&amp;course='.$site->id;
 
             $posttext = get_string('digestmailheader', 'forum', $headerdata)."\n\n";
-            $headerdata->userprefs = '<a target="_blank" href="'.$headerdata->userprefs.'">'.get_string('digestmailprefs', 'forum').'</a>';
+            $headerdata->userprefs = '<a target="_blank" href="'.$headerdata->userprefs.'">'.
+                get_string('digestmailprefs', 'forum').'</a>';
 
             $posthtml = "<head>";
-/*                foreach ($CFG->stylesheets as $stylesheet) {
+             /*   foreach ($CFG->stylesheets as $stylesheet) {
                 //TODO: MDL-21120
                 $posthtml .= '<link rel="stylesheet" type="text/css" href="'.$stylesheet.'" />'."\n";
             }*/
@@ -1036,17 +1037,17 @@ function forum_mail_digests_task() {
 
             foreach ($thesediscussions as $discussionid) {
 
-                core_php_time_limit::raise(120);   // to be reset for each post
+                core_php_time_limit::raise(120);   // To be reset for each post.
 
                 $discussion = $discussions[$discussionid];
                 $forum      = $forums[$discussion->forum];
                 $course     = $courses[$forum->course];
                 $cm         = $coursemodules[$forum->id];
 
-                //override language
+                // Override language.
                 cron_setup_user($userto, $course);
 
-                // Fill caches
+                // Fill caches.
                 if (!isset($userto->viewfullnames[$forum->id])) {
                     $modcontext = context_module::instance($cm->id);
                     $userto->viewfullnames[$forum->id] = has_capability('moodle/site:viewfullnames', $modcontext);
@@ -1064,9 +1065,9 @@ function forum_mail_digests_task() {
                 $posttext .= "\n \n";
                 $posttext .= '=====================================================================';
                 $posttext .= "\n \n";
-                $posttext .= "$shortname -> $strforums -> ".format_string($forum->name,true);
+                $posttext .= "$shortname -> $strforums -> ".format_string($forum->name, true);
                 if ($discussion->name != $forum->name) {
-                    $posttext  .= " -> ".format_string($discussion->name,true);
+                    $posttext  .= " -> ".format_string($discussion->name, true);
                 }
                 $posttext .= "\n";
                 $posttext .= $CFG->wwwroot.'/mod/forum/discuss.php?d='.$discussion->id;
@@ -1075,11 +1076,13 @@ function forum_mail_digests_task() {
                 $posthtml .= "<p><font face=\"sans-serif\">".
                 "<a target=\"_blank\" href=\"$CFG->wwwroot/course/view.php?id=$course->id\">$shortname</a> -> ".
                 "<a target=\"_blank\" href=\"$CFG->wwwroot/mod/forum/index.php?id=$course->id\">$strforums</a> -> ".
-                "<a target=\"_blank\" href=\"$CFG->wwwroot/mod/forum/view.php?f=$forum->id\">".format_string($forum->name,true)."</a>";
+                "<a target=\"_blank\" href=\"$CFG->wwwroot/mod/forum/view.php?f=$forum->id\">".
+                format_string($forum->name, true)."</a>";
                 if ($discussion->name == $forum->name) {
                     $posthtml .= "</font></p>";
                 } else {
-                    $posthtml .= " -> <a target=\"_blank\" href=\"$CFG->wwwroot/mod/forum/discuss.php?d=$discussion->id\">".format_string($discussion->name,true)."</a></font></p>";
+                    $posthtml .= " -> <a target=\"_blank\" href=\"$CFG->wwwroot/mod/forum/discuss.php?d=$discussion->id\">".
+                        format_string($discussion->name, true)."</a></font></p>";
                 }
                 $posthtml .= '<p>';
 
@@ -1089,7 +1092,7 @@ function forum_mail_digests_task() {
                 foreach ($postsarray as $postid) {
                     $post = $posts[$postid];
 
-                    if (array_key_exists($post->userid, $users)) { // we might know him/her already
+                    if (array_key_exists($post->userid, $users)) { // We might know him/her already.
                         $userfrom = $users[$post->userid];
                         if (!isset($userfrom->idnumber)) {
                             $userfrom = $DB->get_record('user', array('id' => $userfrom->id));
@@ -1169,25 +1172,26 @@ function forum_mail_digests_task() {
                 }
                 $footerlinks = array();
                 if ($canunsubscribe) {
-                    $footerlinks[] = "<a href=\"$CFG->wwwroot/mod/forum/subscribe.php?id=$forum->id\">" . get_string("unsubscribe", "forum") . "</a>";
+                    $footerlinks[] = "<a href=\"$CFG->wwwroot/mod/forum/subscribe.php?id=$forum->id\">".
+                        get_string("unsubscribe", "forum") . "</a>";
                 } else {
                     $footerlinks[] = get_string("everyoneissubscribed", "forum");
                 }
-                $footerlinks[] = "<a href='{$CFG->wwwroot}/mod/forum/index.php?id={$forum->course}'>" . get_string("digestmailpost", "forum") . '</a>';
+                $footerlinks[] = "<a href='{$CFG->wwwroot}/mod/forum/index.php?id={$forum->course}'>".
+                    get_string("digestmailpost", "forum") . '</a>';
                 $posthtml .= "\n<div class='mdl-right'><font size=\"1\">" . implode('&nbsp;', $footerlinks) . '</font></div>';
                 $posthtml .= '<hr size="1" noshade="noshade" /></p>';
             }
             $posthtml .= '</body>';
 
             if (empty($userto->mailformat) || $userto->mailformat != 1) {
-                // This user DOESN'T want to receive HTML
+                // This user DOESN'T want to receive HTML.
                 $posthtml = '';
             }
 
-            $attachment = $attachname='';
             // Directly email forum digests rather than sending them via messaging, use the
             // site shortname as 'from name', the noreply address will be used by email_to_user.
-            $mailresult = email_to_user($userto, $site->shortname, $postsubject, $posttext, $posthtml, $attachment, $attachname);
+            $mailresult = email_to_user($userto, $site->shortname, $postsubject, $posttext, $posthtml);
 
             if (!$mailresult) {
                 mtrace("ERROR: mod/forum/cron.php: Could not send out digest mail to user $userto->id ".
@@ -1196,12 +1200,12 @@ function forum_mail_digests_task() {
                 mtrace("success.");
                 $usermailcount++;
 
-                // Mark post as read if forum_usermarksread is set off
+                // Mark post as read if forum_usermarksread is set off.
                 forum_tp_mark_posts_read($userto, $userto->markposts);
             }
         }
     }
-    // We have finishied all digest emails, update $CFG->digestmailtimelast
+    // We have finishied all digest emails, update $CFG->digestmailtimelast.
     set_config('digestmailtimelast', $timenow);
 
     cron_setup_user();
